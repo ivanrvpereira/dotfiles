b651e408f3123bad32a11422f1d0dd72c688d895
Ivan Pereira <ivan@plugidea.pt>
2015-08-02 21:22:37 +0100

    initial commit

---
+++ brew-cask.sh
@@ -0,0 +1,25 @@
+brew install caskroom/cask/brew-cask
+
+brew cask install amethyst
+brew cask install appcleaner
+brew cask install battery-guardian
+brew cask install caffeine
+brew cask install cord
+brew cask install dropbox
+brew cask install evernote
+brew cask install google-chrome
+brew cask install google-chrome-canary
+brew cask install hipchat
+brew cask install iterm2
+brew cask install limechat
+brew cask install quicklook-json
+brew cask install rescuetime
+brew cask install sequel-pro
+brew cask install skype
+brew cask install spotify
+brew cask install the-unarchiver
+brew cask install transmission
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install vlc
+
---
+++ brew.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# Install GNU core utilities (those that come with OS X are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+# Install Bash 4.
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+# running `chsh`.
+brew install bash
+brew tap homebrew/versions
+#brew install bash-completion2
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install RingoJS and Narwhal.
+# Note that the order in which these are installed is important;
+# see http://git.io/brew-narwhal-ringo.
+#brew install ringojs
+#brew install narwhal
+
+# Install more recent versions of some OS X tools.
+brew tap neovim/neovim
+brew install --HEAD neovim
+brew install vim --override-system-vi
+
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+
+# Install font tools.
+brew tap bramstein/webfonttools
+brew install sfnt2woff
+brew install sfnt2woff-zopfli
+brew install woff2
+
+
+# Install other useful binaries.
+brew install ack
+brew install git
+brew install git-flow
+brew install git-lfs
+brew install imagemagick --with-webp
+brew install lynx
+brew install p7zip
+brew install pigz
+brew install pv
+brew install rename
+brew install rhino
+brew install speedtest_cli
+brew install ssh-copy-id
+brew install tree
+brew install webkit2png
+
+
+# languages
+brew install homebrew/php/php55 --with-gmp
+curl https://raw.githubusercontent.com/shawncplus/phpcomplete.vim/master/misc/ctags-better-php.rb > /usr/local/Library/Formula/ctags-better-php.rb
+brew install ctags-better-php
+brew install python
+brew install python3
+
+
+# my tools
+brew install zsh
+brew install the_silver_searcher
+brew install trash # moves files to trash on osx
+brew install boot2docker
+brew install toggle-osx-shadows
+brew install unrar
+brew install htop-osx
+brew install percona-server
+brew install midnight-commander
+brew install wget
+brew install weechat
+brew install collordiff
+brew install tmux
+brew install fasd
+
+# Remove outdated versions from the cellar.
+brew cleanup
