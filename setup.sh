aff27d267e4c907dd8f88b3c3410bb5ab74f9633
Ivan Pereira <183991+ivanrvpereira@users.noreply.github.com>
2015-08-02 21:22:37 +0100

    initial commit

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+DOTDIR=$HOME/.dotfiles
+
+ln -s $DOTDIR/zshrc $HOME/.zshrc
+ln -s $DOTDIR/curlrc $HOME/.curlrc
+ln -s $DOTDIR/gitconfig $HOME/.gitconfig
+ln -s $DOTDIR/gitignore $HOME/.gitignore
+ln -s $DOTDIR/hgignore $HOME/.hgignore
+ln -s $DOTDIR/hgrc $HOME/.hgrc
+ln -s $DOTDIR/tmux.conf $HOME/.tmux.conf
+ln -s $DOTDIR/wgetrc $HOME/.wgetrc
+ln -s $DOTDIR/vimrc.after $HOME/.vimrc.after
+ln -s $DOTDIR/vimrc.before $HOME/.vimrc.before
+ln -s $DOTDIR/dircolors $HOME/.dircolors
+ln -s $DOTDIR/inputrc $HOME/.inputrc
+ln -s $DOTDIR/.janus-modules $HOME/.janus
+ln -s $DOTDIR/oh-my-zsh $HOME/.oh-my-zsh
+
+
+
+#ln -s zshrc ~/.zshrc
+
