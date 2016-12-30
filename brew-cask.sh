275be668ae54b48de4d4b3ef4198cfe93dbd1601
Ivan Pereira <183991+ivanrvpereira@users.noreply.github.com>
2016-12-30 20:24:55 +0000

    update some packages

--- brew-cask.sh
+++ brew-cask.sh
@@ -8,22 +8,25 @@ brew cask install evernote
 brew cask install google-chrome
 brew cask install opera
 #brew cask install opera-developer
+brew cask install slack
 brew cask install hipchat
+brew cask install skype
 brew cask install iterm2
+
 brew cask install limechat
 brew cask install mou
 brew cask install quicklook-json
 brew cask install rescuetime
 brew cask install sequel-pro
-brew cask install skype
 brew cask install spotify
+
 brew cask install the-unarchiver
+brew cask install transmit
 brew cask install transmission
 #brew cask install vagrant
 #brew cask install virtualbox
 brew cask install vlc
-brew cask install transmit
-brew cask install transmission
 brew cask install postman
 brew cask install sourcetree
 brew cask install robomongo
+brew cask install rdm
