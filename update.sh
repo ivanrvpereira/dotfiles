22c962a7cebdedf4822c3df9a84f6e2c56665691
Ivan Pereira <183991+ivanrvpereira@users.noreply.github.com>
2021-04-02 21:59:20 +0100

    update scripts

--- update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-pip install -U -r pip.txt
-brew update && brew upgrade `brew outdated`
-git submodule foreach git pull origin master
-cd ~/.vim && rake
-cd ~/.janus/YouCompleteMe && git submodule update --init --recursive && python install.py --clang-completer --tern-completer
