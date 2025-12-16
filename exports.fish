1c580032a7a31cb679535e4147a67a4210c06393
Ivan Pereira <183991+ivanrvpereira@users.noreply.github.com>
2025-12-16 17:59:28 +0000

    update dependencies and remove hard coded lines

--- exports.fish
+++ exports.fish
@@ -1,6 +1,9 @@
+# Detect Homebrew prefix (Apple Silicon: /opt/homebrew, Intel: /usr/local)
+set -gx HOMEBREW_PREFIX (brew --prefix)
+
 # Path setup (order matters - first takes precedence)
 fish_add_path /Applications/PyCharm.app/Contents/MacOS
-fish_add_path /opt/homebrew/opt/openjdk@17/bin
+fish_add_path $HOMEBREW_PREFIX/opt/openjdk@17/bin
 fish_add_path $HOME/Library/Haskell/bin
 set -gx GOPATH $HOME/go
 fish_add_path $GOPATH/bin
@@ -8,19 +11,19 @@ fish_add_path ~/.vector/bin
 
 fish_add_path ~/.local/bin
 
-fish_add_path /opt/homebrew/opt/coreutils/libexec/gnubin
-fish_add_path /opt/homebrew/sbin
-fish_add_path /opt/homebrew/bin
-fish_add_path /opt/homebrew/opt/libpq/bin
+fish_add_path $HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin
+fish_add_path $HOMEBREW_PREFIX/sbin
+fish_add_path $HOMEBREW_PREFIX/bin
+fish_add_path $HOMEBREW_PREFIX/opt/libpq/bin
 
-set -gx MANPATH /opt/homebrew/opt/coreutils/libexec/gnuman $MANPATH
+set -gx MANPATH $HOMEBREW_PREFIX/opt/coreutils/libexec/gnuman $MANPATH
 
 set -gx ENVIRONMENT development
 
 set -gx LANG en_US.UTF-8
 set -gx LC_ALL en_US.UTF-8
 
-set -gx JAVA_HOME /opt/homebrew/opt/openjdk@17/libexec/openjdk.jdk/Contents/Home
+set -gx JAVA_HOME $HOMEBREW_PREFIX/opt/openjdk@17/libexec/openjdk.jdk/Contents/Home
 set -gx UBI_TOKEN op://development/ubicloud-token/credential
 set -gx SSH_AUTH_SOCK $HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 
