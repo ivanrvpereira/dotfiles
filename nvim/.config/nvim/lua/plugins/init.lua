ee01582ba811fe6c2e6a6b5bc4f2d94e70811ee8
Ivan Pereira <183991+ivanrvpereira@users.noreply.github.com>
2026-06-02 15:30:22 +0100

    feat(nav): use Ctrl-h/j/k/l for vim/tmux pane navigation
    Drop the old Cmd+Alt h/j/k/l Ghostty keybinds and add the
    vim-tmux-navigator spec in Neovim with Ctrl-h/j/k/l mappings
    (normal + terminal mode). Pairs with the tmux.conf switch from
    Alt to Ctrl pane navigation.

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,4 +1,25 @@
 -- Add plugin specs as new .lua files in this directory,
 -- or add them to this file's return table.
 -- See: https://lazy.folke.io/spec
-return {}
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+    init = function()
+      vim.g.tmux_navigator_no_mappings = 1
+    end,
+    config = function()
+      local map = vim.keymap.set
+      local opts = { silent = true }
+
+      map("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", vim.tbl_extend("force", opts, { desc = "Go to Left Window" }))
+      map("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", vim.tbl_extend("force", opts, { desc = "Go to Lower Window" }))
+      map("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", vim.tbl_extend("force", opts, { desc = "Go to Upper Window" }))
+      map("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", vim.tbl_extend("force", opts, { desc = "Go to Right Window" }))
+
+      map("t", "<C-h>", [[<C-w>:<C-U>TmuxNavigateLeft<cr>]], opts)
+      map("t", "<C-j>", [[<C-w>:<C-U>TmuxNavigateDown<cr>]], opts)
+      map("t", "<C-k>", [[<C-w>:<C-U>TmuxNavigateUp<cr>]], opts)
+      map("t", "<C-l>", [[<C-w>:<C-U>TmuxNavigateRight<cr>]], opts)
+    end,
+  },
+}
